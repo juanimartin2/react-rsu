8030eba8c9e43fb088e0657d115b8d5a12ee37e0
juanimartin2 <juanimartinnn3@gmail.com>
2024-10-25 19:58:55 -0300

    fix tailwind

--- componentes/Formulario.tsx
+++ componentes/Formulario.tsx
@@ -5,13 +5,21 @@ function Formulario() {
   return (
     <form className="flexCenter flex-col gap-3 p-6" id='login'>
       <>
-        <label className='rounded-lg border-2 border-slate-400 flex-col'>
-          Usuario
-          <input type="email" name="usuario" />
+        <label>
+        <h3>Usuario</h3>
+          <input 
+          className='rounded-lg border-2 border-slate-400' 
+          type="email" 
+          name="usuario" 
+          />
         </label>
-        <label className='rounded-lg border-2 border-slate-400'>
-          Clave
-          <input type="text" name="clave" />
+        <label>
+          <h3>Clave</h3>
+          <input 
+          className='rounded-lg border-2 border-slate-400'
+          type="text" 
+          name="clave" 
+          />
         </label>
         <Button
           type={"submit"}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  mode: 'jit',
+  purge: [
+    './pages//*.{js,ts,jsx,tsx,mdx}',
+    './componentes//*.{js,ts,jsx,tsx,mdx}',
+    './app//*.{js,ts,jsx,tsx,mdx}',
+    "./views//*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
